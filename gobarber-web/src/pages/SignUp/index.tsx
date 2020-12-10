3844417a2deae3ccb6d58ce7cb88371467175ebc
Renan <stdio@MacBook-Pro-de-Renan.local>
2020-12-09 21:09:30 -0300

    exibindo erros no input

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,25 +1,37 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import { Container, Content, BackGround } from './style';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErros from '../../utils/getValidationErrors';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'No minimo 6 caracteres'),
+        password: Yup.string().min(6, 'Minimo de 6 caracteres'),
       });
-      await schema.validate(data, { abortEarly: false });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
     } catch (err) {
       console.log(err);
+
+      const errors = getValidationErros(err);
+      console.log(errors);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -30,9 +42,9 @@ const SignUp: React.FC = () => {
         <Content>
           <img src={logoImg} alt="logo GoBarber" />
 
-          <Form onSubmit={handleSubmit}>
+          <Form ref={formRef} onSubmit={handleSubmit}>
             <h1> Faça seu cadastro</h1>
-            <Input name="nome" icon={FiUser} type="text" placeholder="Nome" />
+            <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
             <Input
               name="email"
               icon={FiMail}
